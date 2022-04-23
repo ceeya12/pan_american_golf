3713a86423cea035ffa6899f9d4d93921891cb0d
ceeya12 <62365594+ceeya12@users.noreply.github.com>
2022-04-23 11:28:49 -0500

    Commmit and Push

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import flask
 from flask import Flask
 from flask import render_template
+from flask import send_from_directory
 
 
 app = Flask(__name__)
@@ -7,11 +9,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():  # put application's code here
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/menshallofame')
-def mensHOF(): # put application's code here
-    return render_template('menshalloffame.html')
+@app.route('/menshalloffame')
+def menshalloffame(): # put application's code here
+    return flask.render_template("menshalloffame.html")
 
 
 if __name__ == '__main__':
